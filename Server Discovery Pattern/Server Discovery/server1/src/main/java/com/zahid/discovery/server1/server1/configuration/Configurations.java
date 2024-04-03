3edcfc2f658aeb841fa9a2230256975288e3f017
Zahid_Khan <xahiidkhan@gmail.com>
2024-04-03 13:53:39 +0530

    Service Registry and Service Discovery.

---
+++ Server Discovery Pattern/Server Discovery/server1/src/main/java/com/zahid/discovery/server1/server1/configuration/Configurations.java
@@ -0,0 +1,15 @@
+package com.zahid.discovery.server1.server1.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class Configurations{
+	@Bean
+	@LoadBalanced
+    RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
---
+++ Server Discovery Pattern/Server Discovery/server1/src/main/java/com/zahid/discovery/server1/server1/controller/MainController.java
@@ -0,0 +1,2 @@
+package com.zahid.discovery.server1.server1.controller;public class MainController {
+}
