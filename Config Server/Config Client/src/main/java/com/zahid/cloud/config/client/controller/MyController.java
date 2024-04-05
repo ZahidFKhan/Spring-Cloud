b2726873f9e5eeababb19faa2c5b0a9dfe568cd9
Zahid_Khan <xahiidkhan@gmail.com>
2024-04-05 11:10:56 +0530

    Spring Client Config updated

--- Config Server/Config Client/src/main/java/com/zahid/cloud/config/client/controller/MyController.java
+++ Config Server/Config Client/src/main/java/com/zahid/cloud/config/client/controller/MyController.java
@@ -1,17 +1,14 @@
 package com.zahid.cloud.config.client.controller;
 
 import com.zahid.cloud.config.client.config.Config;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class MyController {
   private final Config config;
-
-  public MyController(Config config) {
-    this.config = config;
-  }
 
   @GetMapping
   String returnValue() {
