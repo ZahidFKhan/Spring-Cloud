5a4ad4c864d96cd5eebdeefe0d3c36eca46b27b3
Zahid_Khan <xahiidkhan@gmail.com>
2024-04-13 11:44:27 +0530

    Consumer Driven Contracts

--- ConsumerDrivenContract/ContractConsumer/src/main/java/com/zahid/contract/consumer/contractconsumer/entities/Customer.java
+++
@@ -1,4 +0,0 @@
-package com.zahid.contract.consumer.contractconsumer.entities;
-
-
-public record Customer(int id, String name) {}
--- ConsumerDrivenContract/Producer/src/main/java/com/zahid/contract/ConsumerDrivenContract/entities/Customer.java
+++
@@ -1,9 +0,0 @@
-package com.zahid.contract.ConsumerDrivenContract.entities;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "customer")
-public record Customer(@Id int id, String name) {}
