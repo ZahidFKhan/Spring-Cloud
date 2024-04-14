df27e8cca8e73bfa7fe24dca599a0186be77a537
Zahid_Khan <xahiidkhan@gmail.com>
2024-04-14 21:46:07 +0530

    Name changes

---
+++ Api Gateway/src/main/java/com/zahid/api/gateway/Apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zahid.api.gateway.Apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
